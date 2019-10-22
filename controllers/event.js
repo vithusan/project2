c327f465b02cb5ed646a613dafff04220ac25e2c
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-10-22 12:15:25 -0400

    updated event controller to render event handlebars

--- controllers/event.js
+++ controllers/event.js
@@ -2,38 +2,54 @@ const express = require('express')
 const eventApi = require('../models/event.js')
 const eventRouter = express.Router()
 
+eventRouter.get('/event/new', (req, res) => {
+    res.render('event/createEvent')
+})
+
+eventRouter.get('/event/edit/:id', (req, res) =>{
+    eventApi.getSingleEvent(req.params.id)
+    .then((updateSingleEvent) => {
+        res.render('event/updateEvent', {updateSingleEvent})
+    })
+})
+
 eventRouter.get('/event', (req, res) => {
     eventApi.getAllEvent()
     .then((allEvent) => {
-        res.json(allEvent)
+        // res.json(allEvent)
+        res.render('event/allEvent', {allEvent})
     })
 })
 
 eventRouter.get('/event/:id', (req, res) => {
     eventApi.getSingleEvent(req.params.id)
     .then((singleEvent) => {
-        res.json(singleEvent)
+        // res.json(singleEvent)
+        res.render('event/singleEvent', {singleEvent})
     })
 })
 
 eventRouter.post('/event', (req, res) => {
     eventApi.createEvent(req.body)
     .then((createdEvent) => {
-        res.json(createdEvent)
+        // res.json(createdEvent)
+        res.redirect('/event')
     })
 })
 
 eventRouter.put('/event/:id', (req, res) => {
     eventApi.updateEvent(req.params.id, req.body)
     .then((updatedEvent) => {
-        res.json(updatedEvent)
+        // res.json(updatedEvent)
+        res.redirect(`/event/${req.params.id}`)
     })
 })
 
 eventRouter.delete('/event/:id', (req, res) => {
     eventApi.deleteEvent(req.params.id)
     .then((deletedEvent) => {
-        res.json(deletedEvent)
+        // res.json(deletedEvent)
+        res.redirect('/event')
     })
 })
 
