d3b947db78d2afb93d6d547efea18c7181d40e05
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-10-22 11:50:02 -0400

    updated the car controllers to render handlebars

--- controllers/car.js
+++ controllers/car.js
@@ -2,38 +2,53 @@ const express = require('express')
 const carApi = require('../models/car.js')
 const carRouter = express.Router()
 
+carRouter.get('/car/new', (req, res) => {
+    res.render('car/createCar')
+})
+
+carRouter.get('/car/edit/:id', (req, res) => {
+    carApi.getSingleCar(req.params.id)
+    .then((singleCar) => {
+        res.render('car/updateCar', {singleCar})
+    })
+})
 carRouter.get('/car', (req, res) => {
     carApi.getAllCar()
     .then((allCar) => {
-        res.json(allCar)
+        // res.json(allCar)
+        res.render('car/allCar', {allCar})
     })
 })
 
 carRouter.get('/car/:id', (req, res) => {
     carApi.getSingleCar(req.params.id)
     .then((singleCar) => {
-        res.json(singleCar)
+        // res.json(singleCar)
+        res.render('car/singleCar', {singleCar})
     })
 })
 
 carRouter.post('/car' , (req, res) => {
     carApi.createCar(req.body)
     .then((createdCar) => {
-        res.json(createdCar)
+        // res.json(createdCar)
+        res.redirect('/car')
     })
 })
 
 carRouter.put('/car/:id' , (req, res) => {
     carApi.updateCar(req.params.id, req.body)
     .then((updatedCar) => {
-        res.json(updatedCar)
+        // res.json(updatedCar)
+        res.redirect(`/car/${req.params.id}`)
     })
 })
 
 carRouter.delete('/car/:id' , (req, res) => {
     carApi.deleteCar(req.params.id)
     .then((deletedCar) => {
-        res.json(deletedCar)
+        // res.json(deletedCar)
+        res.redirect('/car')
     })
 })
 
